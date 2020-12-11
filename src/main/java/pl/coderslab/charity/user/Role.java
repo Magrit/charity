cf71a338987f77198b0a129160bb90f872eb983d
Gosia Jedrzejewska <gorzata.jedrzejewska@gmail.com>
2020-12-11 17:06:15 +0100

    role

---
+++ src/main/java/pl/coderslab/charity/user/Role.java
@@ -0,0 +1,15 @@
+package pl.coderslab.charity.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}
